6c8d63d3728a8e77461ea5f853851ede79390030
Rohail Taimour <rohail.taimour@gmail.com>
2026-03-14 01:40:52 +0100

    A nicer git log viewer

--- .config/shell/fzf_conf/.fzf-git-config.sh
+++ .config/shell/fzf_conf/.fzf-git-config.sh
@@ -2,18 +2,46 @@
 # https://github.com/junegunn/fzf-git.sh
 # Log view
 fzf_git_log() {
-    local selection=$(
-      git log --oneline  --color=always "$@" | \
-        fzf --no-multi --ansi --no-sort --no-height \
-            --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
-                       xargs -I@ sh -c 'git show --color=always @'"
-    )
-    if [[ -n $selection ]]; then
-        local commit=$(echo "$selection" | sed 's/^[* |]*//' | awk '{print $1}' | tr -d '\n')
-        git show $commit
-    fi
-}
+    local selection
+    local commit
+    local root_dir
 
+    root_dir=$(git rev-parse --show-toplevel 2>/dev/null) || return
+
+    selection=$(
+      git log \
+          --graph \
+          --format='%C(auto)%h %d %s %C(black)%C(bold)%cr' \
+          --color=always \
+          "$@" | \
+        fzf \
+          --ansi \
+          --no-sort \
+          --reverse \
+          --no-multi \
+          --preview '
+              commit=$(echo {} | grep -o "[a-f0-9]\{7,\}" | head -1)
+              echo "FILES:"
+              git diff-tree --no-commit-id --name-only -r $commit
+              echo 
+              echo "STATS:"
+              git show --stat --oneline $commit
+              echo
+              echo "PATCH:"
+              git show --color=always $commit
+          '
+    )
+
+    # If the user made a selection (didn't hit ESC)
+    if [[ -n "$selection" ]]; then
+        # Extract the commit hash
+        commit=$(echo "$selection" | grep -o "[a-f0-9]\{7,\}" | head -1)
+        
+        # Open Vim, change to root directory, and run Fugitive's difftool
+        # We use -c "cmd" for the first command and -c "cmd" for the second
+        vim -c "cd $root_dir" -c "Git difftool -y ${commit}^ $commit"
+    fi  
+}
 alias gll='fzf_git_log'
 
 # pick axe
