73c83b1ff11794961f802d4a3e5abce7c26ce530
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-18 01:29:07 +0200

    Add removal of binding to allow this to work

--- .config/shell/os/macos.sh
+++ .config/shell/os/macos.sh
@@ -12,12 +12,15 @@ alias reload='source ~/.zshrc'
 # Allow comments
 setopt interactivecomments
 
+# source_if_exists /usr/share/bash-completion/completions/git
 source <(fzf --zsh)
 eval "$(rbenv init - zsh)"
 eval "$(starship init zsh)"
 # brew install zsh-vi-mode
 source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
+# bindkey '^G' list-expand prevents binding to work on mac, hence removing this here
+bindkey -r '^G'
 tmux-window-name() {
   ($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
 }
