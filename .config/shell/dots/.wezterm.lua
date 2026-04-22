e0b4a7b0d556a204675ded2fe0c9dccffdadf2c9
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-23 01:18:18 +0200

    Bug

--- .config/shell/dots/.wezterm.lua
+++ .config/shell/dots/.wezterm.lua
@@ -63,11 +63,11 @@ local my_keys = {
   { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Left" },
   { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Down" },
   { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Up" },
-  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right" }
+  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right" },
 
   -- Swapping Windows: https://github.com/sei40kr/wez-pain-control/blob/main/plugin/init.lua
-  { key = "<", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-  { key = ">", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+  { key = "<", mods = "LEADER|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
+  { key = ">", mods = "LEADER|SHIFT", action = wezterm.action.MoveTabRelative(1) }
 }
 
 for _, key in ipairs(my_keys) do
