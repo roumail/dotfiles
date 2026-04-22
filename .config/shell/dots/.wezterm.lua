51715db4167000fd7dbde072296db0fffbed2e7c
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-22 21:18:18 +0200

    Add basic wezterm

---
+++ .config/shell/dots/.wezterm.lua
@@ -0,0 +1,49 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- config.color_scheme = 'Batman'
+config.font_size = 12
+config.font = wezterm.font 'JetBrains Mono'
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.keys = {
+  -- splits
+  {
+    key = "|",
+    mods = "LEADER|SHIFT",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "-",
+    mods = "LEADER",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+
+  -- new tab
+  {
+    key = "c",
+    mods = "LEADER",
+    action = wezterm.action.SpawnTab "CurrentPaneDomain",
+  },
+
+  -- navigation
+  { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Left" },
+  { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Down" },
+  { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Up" },
+  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right" },
+
+  -- rename tab
+  {
+    key = ",",
+    mods = "LEADER",
+    action = wezterm.action.PromptInputLine {
+      description = "Rename tab",
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+}
+return config
