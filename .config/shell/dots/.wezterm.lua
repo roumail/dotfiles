125227205c692782c1e846fc040493abb9efbfbb
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-23 00:49:19 +0200

    Add version that adds tmux

--- .config/shell/dots/.wezterm.lua
+++ .config/shell/dots/.wezterm.lua
@@ -1,12 +1,29 @@
 local wezterm = require 'wezterm'
-local config = {}
+local config = wezterm.config_builder()
+
+local function remove_key(keys, key, mods)
+  local result = {}
+  for _, k in ipairs(keys) do
+    if not (k.key == key and k.mods == mods) then
+      table.insert(result, k)
+    end
+  end
+  return result
+end
 
 -- config.color_scheme = 'Batman'
 config.font_size = 12
 config.font = wezterm.font 'JetBrains Mono'
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+-- config.disable_default_key_bindings = true
 
-config.keys = {
+-- load plugin
+local wez_tmux = require("plugins.wez-tmux.plugin")
+wez_tmux.apply_to_config(config)
+config.keys = remove_key(config.keys, "%", "LEADER|SHIFT")
+config.keys = remove_key(config.keys, "\"", "LEADER|SHIFT")
+
+local my_keys = {
   -- splits
   {
     key = "|",
@@ -19,31 +36,16 @@ config.keys = {
     action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
   },
 
-  -- new tab
-  {
-    key = "c",
-    mods = "LEADER",
-    action = wezterm.action.SpawnTab "CurrentPaneDomain",
-  },
-
   -- navigation
   { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Left" },
   { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Down" },
   { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Up" },
-  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right" },
-
-  -- rename tab
-  {
-    key = ",",
-    mods = "LEADER",
-    action = wezterm.action.PromptInputLine {
-      description = "Rename tab",
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    },
-  },
+  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right" }
+  --   key = "s", mods = "LEADER",action = wezterm.action.ShowLauncherArgs {flags = "FUZZY|WORKSPACES"}
 }
+
+for _, key in ipairs(my_keys) do
+  table.insert(config.keys, key)
+end
+
 return config
