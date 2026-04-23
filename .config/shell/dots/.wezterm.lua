cb0c8da044a785381a46dd4293153b0e670fcce9
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-23 12:05:44 +0200

    Add guard for wsl specific setup

--- .config/shell/dots/.wezterm.lua
+++ .config/shell/dots/.wezterm.lua
@@ -75,36 +75,7 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     },
   }
   config.default_domain = "WSL:Debian"
-  config.wsl_domains = {
-    {
-      -- The name of this specific domain.  Must be unique amonst all types
-      -- of domain in the configuration file.
-      name = 'WSL:Debian',
-
-      -- The name of the distribution.  This identifies the WSL distribution.
-      -- It must match a valid distribution from your `wsl -l -v` output in
-      -- order for the domain to be useful.
-      distribution = 'Debian',
-
-      -- The username to use when spawning commands in the distribution.
-      -- If omitted, the default user for that distribution will be used.
-
-      -- username = "hunter",
-
-      -- The current working directory to use when spawning commands, if
-      -- the SpawnCommand doesn't otherwise specify the directory.
-
-      default_cwd = "~",
-
-      -- The default command to run, if the SpawnCommand doesn't otherwise
-      -- override it.  Note that you may prefer to use `chsh` to set the
-      -- default shell for your user inside WSL to avoid needing to
-      -- specify it here
-
-      -- default_prog = {"fish"}
-    },
-  }
-  config.default_domain = "WSL:Debian"
+  -- config.default_prog = { 'C:\\Windows\\System32\\wsl.exe', '--distribution', 'Debian', '--cd', '~' }
 end
 
 local my_keys = {
