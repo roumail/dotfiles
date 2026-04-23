2347b5ae364413cb5275e436645d82a6a1bb9c33
Rohail Taimour <rohail.taimour@gmail.com>
2026-04-23 11:12:34 +0200

    Add windows version of wsl

--- .config/shell/dots/.wezterm.lua
+++ .config/shell/dots/.wezterm.lua
@@ -44,6 +44,69 @@ config.keys = remove_key(config.keys, "%", "LEADER|SHIFT")
 config.keys = remove_key(config.keys, "\"", "LEADER|SHIFT")
 config.keys = remove_key(config.keys, "l", "LEADER")
 
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.wsl_domains = {
+    {
+      -- The name of this specific domain.  Must be unique amonst all types
+      -- of domain in the configuration file.
+      name = 'WSL:Debian',
+
+      -- The name of the distribution.  This identifies the WSL distribution.
+      -- It must match a valid distribution from your `wsl -l -v` output in
+      -- order for the domain to be useful.
+      distribution = 'Debian',
+
+      -- The username to use when spawning commands in the distribution.
+      -- If omitted, the default user for that distribution will be used.
+
+      -- username = "hunter",
+
+      -- The current working directory to use when spawning commands, if
+      -- the SpawnCommand doesn't otherwise specify the directory.
+
+      default_cwd = "~",
+
+      -- The default command to run, if the SpawnCommand doesn't otherwise
+      -- override it.  Note that you may prefer to use `chsh` to set the
+      -- default shell for your user inside WSL to avoid needing to
+      -- specify it here
+
+      -- default_prog = {"fish"}
+    },
+  }
+  config.default_domain = "WSL:Debian"
+  config.wsl_domains = {
+    {
+      -- The name of this specific domain.  Must be unique amonst all types
+      -- of domain in the configuration file.
+      name = 'WSL:Debian',
+
+      -- The name of the distribution.  This identifies the WSL distribution.
+      -- It must match a valid distribution from your `wsl -l -v` output in
+      -- order for the domain to be useful.
+      distribution = 'Debian',
+
+      -- The username to use when spawning commands in the distribution.
+      -- If omitted, the default user for that distribution will be used.
+
+      -- username = "hunter",
+
+      -- The current working directory to use when spawning commands, if
+      -- the SpawnCommand doesn't otherwise specify the directory.
+
+      default_cwd = "~",
+
+      -- The default command to run, if the SpawnCommand doesn't otherwise
+      -- override it.  Note that you may prefer to use `chsh` to set the
+      -- default shell for your user inside WSL to avoid needing to
+      -- specify it here
+
+      -- default_prog = {"fish"}
+    },
+  }
+  config.default_domain = "WSL:Debian"
+end
+
 local my_keys = {
   -- splits
   --   key = "s", mods = "LEADER",action = wezterm.action.ShowLauncherArgs {flags = "FUZZY|WORKSPACES"}
